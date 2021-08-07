e614ee52a0fd4bf85ba4fd3a1e4f4524f7046d42
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2021-08-07 10:15:07 +0100

    :wrench:

--- src/views/Home/TransactionHistory/TransactionHistory.tsx
+++ src/views/Home/TransactionHistory/TransactionHistory.tsx
@@ -6,13 +6,48 @@ import { HomeNavigationProps } from '../../../Navigation'
 import Graph, { DataPoint } from './Graph';
 
 const data: DataPoint[] = [
-  { date: new Date('2019-02-08').getTime(), color: "lightblue", value: 0 },
-  { date: new Date('2019-05-01').getTime(), color: "green", value: 140 },
-  { date: new Date('2019-09-14').getTime(), color: "orange", value: 223.7 },
-  { date: new Date('2019-12-10').getTime(), color: "pink", value: 0 },
-  { date: new Date('2020-01-31').getTime(), color: "secondary", value: 190.5 },
-  { date: new Date('2020-07-14').getTime(), color: "violet", value: 101.9 },
-  { date: new Date('2020-08-22').getTime(), color: "danger", value: 42.3 },
+  { 
+    date: new Date('2019-02-08').getTime(),
+    color: "lightblue",
+    value: 0,
+    step: 0,
+  },
+  { 
+    date: new Date('2019-05-01').getTime(),
+    color: "green",
+    value: 140,
+    step: 0,
+  },
+  { 
+    date: new Date('2019-09-14').getTime(),
+    color: "orange",
+    value: 223.7,
+    step: 0,
+  },
+  { 
+    date: new Date('2019-12-10').getTime(),
+    color: "pink",
+    value: 0,
+    step: 0,
+  },
+  { 
+    date: new Date('2020-01-31').getTime(),
+    color: "secondary",
+    value: 190.5,
+    step: 0,
+  },
+  { 
+    date: new Date('2020-07-14').getTime(),
+    color: "violet",
+    value: 101.9,
+    step: 0,
+  },
+  { 
+    date: new Date('2020-08-22').getTime(),
+    color: "danger",
+    value: 42.3,
+    step: 0,
+  },
 ];
 
 
